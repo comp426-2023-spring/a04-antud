1bba5e364b509eb07fc448c9bbf8867f20031330
antud <atudela@unc.edu>
2023-03-31 08:03:41 -0400

    added first two endpoints

--- server.js
+++ server.js
@@ -10,3 +10,18 @@ const args = minimist(process.argv.slice(2));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+
+// Format copied from database in-class exercise :p
+
+// READ (HTTP method GET) at root endpoint /app/
+// Check endpoint at /app/ that returns 200 OK
+app.get("/app/", (req, res, next) => {
+    res.json({"message":"Your API works! (200) :)"});
+	res.status(200);
+});
+
+// Endpoint /app/rps/ that returns {"player":"(rock|paper|scissors)"}
+app.get("/app/rps/", (req, res) => {
+    res.status(200).json(rps());
+});
+
